51f3a438922f33ea2c683c7e5206f888323cc795
kazaxx <haligali025@gmail.com>
2023-06-15 15:48:25 +0300

    очень сильно изменил код добавил правильный вывод pdf файла

--- inn_bot_telega/main.py
+++ inn_bot_telega/main.py
@@ -1,103 +1,89 @@
-import requests
 import telebot
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+import requests
+import re
+from requests.cookies import RequestsCookieJar
+import base64
 
-
-def get_api(INN):
-    client = requests.session()
-    login_url = "https://basis.myseldon.com/api/rest/login"
-    api_url = f"https://basis.myseldon.com/api/rest/find_company?Inn={INN}"
-    logout_url = "https://basis.myseldon.com/api/rest/logout"
-
-    login_data = {
-        "UserName": "test00590736@test.ru",
-        "Password": "YeVgM61u"
-    }
-
-    response = client.post(login_url, data=login_data)
-
-    session_guid = None
-    login_myseldon = None
-    if "Set-Cookie" in response.headers:
-        cookies = response.headers["Set-Cookie"].split(", ")
-        for cookie in cookies:
-            if cookie.startswith("SessionGuid"):
-                session_guid = cookie.split("=")[1]
-            elif cookie.startswith("LoginMyseldon"):
-                login_myseldon = cookie.split("=")[1]
-
-    headers = {}
-    if session_guid:
-        headers["SessionGuid"] = session_guid
-    if login_myseldon:
-        headers["LoginMyseldon"] = login_myseldon
-
-    response = client.get(api_url, headers=headers)
-    json_data = response.json()
-
-    if json_data["status"]["itemsFound"] == 0:
-        return "Компания с таким ИНН не найдена"
-
-    msg = ""
-    company = json_data["companies_list"][0]
-
-    msg += "Коды \n \n"
-    msg += f"   ОГРН - {company['basic']['ogrn']} \n"
-    msg += f"   ИНН - {company['basic']['ogrn']} \n"
-    msg += "Наименовение  \n"
-    msg += f"   Полное - {company['basic']['fullName']} \n"
-    msg += f"   Сокращенное - {company['basic']['shortName']}\n"
-    msg += f"Тел - {company['phoneFormattedList'][0]['number']}\n"
-    msg += f"Адрес - {company['address']}\n"
-    print(msg)
-    return msg
-
-def generate_pdf(company_info):
-    pdf_file_path = "company_info.pdf"
-    c = canvas.Canvas(pdf_file_path, pagesize=letter)
-
-    # Настройки для текста
-    text_x = 50
-    text_y = 700
-    line_height = 20
-    pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
-    # Записываем данные из company_info в PDF
-    c.setFont("Arial", 12)
-    lines = company_info.split("\n")
-    for line in lines:
-        c.drawString(text_x, text_y, line)
-        text_y -= line_height
-
-    c.save()
-    return pdf_file_path
-
-
-bot = telebot.TeleBot('6074017992:AAFOhnU5cQfKaGpzF7ZK32bZKqTB9BBf9_o')
-
+bot_token = "6062411217:AAGTS69Lfg1mbjL3QPNMeC5_Mly6ehR2EyE"
+bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message,
-                 "Привет! Я бот, который может отправить справку о компании по ИНН. Просто отправь мне ИНН компании.")
+def start_command(message):
+    bot.send_message(message.chat.id, 'Привет, я бот для получения информации о компании по ИНН.')
 
+
+
+@bot.message_handler(regexp=r'^\d{10,12}|\d{14,15}$')
+def handle_inn(message):
+    inn = re.sub(r'\s+', '', message.text)
+    bot.send_message(message.chat.id, 'Поиск компании по ИНН...')
+    bot.send_message(message.chat.id, check_inn(inn, message.chat.id))
+
+
+def check_inn(inn, chat_id):
+    session_guid = None
+    login_myseldon = None
+    login_payload = {
+        'UserName': 'test00590736@test.ru',
+        'Password': 'YeVgM61u'
+    }
+
+    login_url = 'https://basis.myseldon.com/api/rest/login'
+    order_url = f'https://basis.myseldon.com/api/rest/order_excerpt_pdf?inn={inn}'
+
+    client = requests.Session()
+    login_response = client.post(login_url, data=login_payload)
+
+    if login_response.status_code == 200:
+        cookies = login_response.cookies
+        session_guid = cookies.get('SessionGuid')
+        login_myseldon = cookies.get('LoginMyseldon')
+
+    if session_guid and login_myseldon:
+        headers = {
+            'SessionGuid': session_guid,
+            'LoginMyseldon': login_myseldon
+        }
+        order_response = client.get(order_url, headers=headers)
+        order_data = order_response.json()
+
+        if 'status' in order_data and order_data['status']['methodStatus'] == 'Error':
+            return f"Компания с таким ИНН не найдена. {order_data['status']['name']}"
+        else:
+            return get_api_pdf(inn, order_data, client, headers, chat_id)
+    else:
+        return "Ошибка входа в API."
+
+def get_api_pdf(inn, order_data, client, headers, chat_id):
+    file_name = "company.pdf"
+
+    order_num = order_data['orderNum']
+
+    while True:
+        res = client.get(f'https://basis.myseldon.com/api/rest/get_excerpt_pdf?orderNum={order_num}', headers=headers)
+        res_data = res.json()
+
+        if 'excerpt_body' in res_data and res_data['excerpt_body'] is not None:
+            break
+
+        print("retry")
+
+    file_name = "company.pdf"
+    with open(file_name, 'wb') as file:
+        file.write(base64.decodebytes(res_data['excerpt_body'].encode()))
+
+    send_file(file_name, chat_id)
+
+    return "Готово!"
+
+def send_file(file_name, chat_id):
+    with open(file_name, 'rb') as file:
+        bot.send_document(chat_id, file)
 
 @bot.message_handler(func=lambda message: True)
-def send_company_info(message):
-    INN = message.text
-    company_info = get_api(INN)
-
-    if company_info.startswith("Компания с таким ИНН не найдена"):
-        bot.reply_to(message, company_info)
-    else:
-        pdf_file_path = generate_pdf(company_info)
-        with open(pdf_file_path, 'rb') as f:
-            bot.send_document(message.chat.id, f)
-
-        # Удалите PDF-файл
-        # os.remove(pdf_file_path)
+def handle_other(message):
+    bot.send_message(message.chat.id, 'ИНН указан неверно.')
 
 
 bot.polling()
+
